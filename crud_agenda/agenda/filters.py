be3c32786287de46c735534301f05401b83df5f8
André Costa <andrelbc7@hotmail.com>
2020-10-16 21:19:11 -0300

    ajustes na função de busca

--- crud_agenda/agenda/filters.py
+++ crud_agenda/agenda/filters.py
@@ -1,11 +1,15 @@
 import django_filters
+from django.db.models import Q
 from django_filters import filters
 
 from .models import *
 
 
 class ContatoFilter(django_filters.FilterSet):
-    email = filters.CharFilter(label='Buscar por email ')
+    email = filters.CharFilter(label='Buscar Contato ', method='customFilter')
     class Meta:
         model = Contato
         fields = ['email']
+
+    def customFilter(self, queryset, name, value):
+        return Contato.objects.filter(Q(nome__icontains=value) | (Q(email__icontains=value)))
